fb2f595d5e455b0d56e5852b34c499a45052b9af
mozuniga93 <mzunigaa@ucenfotec.ac.cr>
2022-03-12 18:24:36 -0600

    bike table

--- src/components/bike/BikeList.js
+++ src/components/bike/BikeList.js
@@ -1,16 +1,69 @@
 import React, { useEffect, useState } from 'react';
 import {useQuery, gql} from '@apollo/client'
 import { GET_ALL_BIKES } from './services/graphql/Queries'
+import "antd/dist/antd.css";
+import { Table } from 'antd';
 
-function BikeList(){
+const columns = [
+    {
+        title: "Number",
+        width: 100,
+        dataIndex: "id",
+        key: "id",
+        fixed: "left",
+    },
+    {
+        title: "Model",
+        width: 100,
+        dataIndex: "model",
+        key: "model",
+        fixed: "left",
+    },
+    {
+        title: "Year",
+        dataIndex: "year",
+        key: "year",
+        width: 150,
+    },
+    {
+        title: "Price",
+        dataIndex: "price",
+        key: "price",
+        width: 150,
+    },
+  ];
 
-const { error, loading, data } = useQuery(GET_ALL_BIKES);
+  const getBikes = (bikesData) => {
+    const data = [];
+    console.log("entro a get bikes")
+    bikesData?.bikes.map((bike) =>
+      data.push({
+        key: bike?.id,
+        id: bike?.id,
+        model: bike?.model,
+        year: bike?.year,
+        price: bike?.price,
+      })
+    );
+    return data;
+  };
 
-useEffect(()=> {
-    console.log(data);
-},[data])
-    return <div>
-    </div>
-}
+    const BikeList = ()=> {
+
+      const AllBikes = () => {
+        const { loading, error, data } = useQuery(GET_ALL_BIKES);
+        console.log(data);
+        
+            if (loading) return <h2>Loading...</h2>;
+            if (error) return  <h2>`Error! ${error.message}`</h2>
+      
+        return <Table columns={columns} datasource={()=>getBikes(data)}/>;
+      };
+
+        return (
+            <AllBikes/>
+        )
+      }
+
 
 export default BikeList;
--- src/components/bike/services/graphql/Queries.js
+++ src/components/bike/services/graphql/Queries.js
@@ -1,17 +1,18 @@
 import { gql } from "@apollo/client";
 
 export const GET_ALL_BIKES = gql`
-  query{
-    allBikes{
-      model
-      year
-      price
-      bikeType{
-        name
-      }
-      manufacturer{
-        name
-      }
+  query getAllBikes{
+    bikes {
+        id
+        model
+        year
+        price
+        bikeType{
+          name
+        }
+        manufacturer{
+          name
+        }
     }
   }
 `;
